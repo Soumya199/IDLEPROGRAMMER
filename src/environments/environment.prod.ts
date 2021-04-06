585af318ff502cb0153ecde3cfed3f55971873d2
Soumya <subhasoumya20@gmail.com>
2021-04-06 10:47:48 +0530

    api disabled

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+ /* production: true,
   firebaseConfigs : {
    apiKey: "AIzaSyD47vcYqVJgWHzqLUk8MY_CHKWSJnjvo8k",
    authDomain: "idleprogrammer-cce18.firebaseapp.com",
@@ -7,7 +7,8 @@ export const environment = {
    projectId: "idleprogrammer-cce18",
    storageBucket: "idleprogrammer-cce18.appspot.com",
    messagingSenderId: "154341107457",
-   appId: "1:154341107457:web:44d8304da8b0bb4541dd00",
+   //appId: "1:154341107457:web:44d8304da8b0bb4541dd00",
    measurementId: "G-DQVSGF8ZCX"
  }
+*/
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+/*export const environment = {
   production: false,
    firebaseConfigs : {
     apiKey: "AIzaSyD47vcYqVJgWHzqLUk8MY_CHKWSJnjvo8k",
@@ -11,11 +11,11 @@ export const environment = {
     projectId: "idleprogrammer-cce18",
     storageBucket: "idleprogrammer-cce18.appspot.com",
     messagingSenderId: "154341107457",
-    appId: "1:154341107457:web:44d8304da8b0bb4541dd00",
+    //appId: "1:154341107457:web:44d8304da8b0bb4541dd00",
     measurementId: "G-DQVSGF8ZCX"
   }
 };
-
+*/
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
